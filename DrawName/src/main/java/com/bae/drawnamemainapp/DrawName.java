6f69aeb647787fd196ea27385ac5c82ac0f97b5e
Monika-Mistry <monika.mistry1@gmail.com>
2019-05-29 12:35:07 +0100

    Add drawname project

---
+++ DrawName/src/main/java/com/bae/drawnamemainapp/DrawName.java
@@ -0,0 +1,10 @@
+package com.bae.drawnamemainapp;
+
+public class DrawName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ DrawName/src/test/java/com/bae/drawnametest/DrawNameTest.java
@@ -0,0 +1,5 @@
+package com.bae.drawnametest;
+
+public class DrawNameTest {
+
+}
