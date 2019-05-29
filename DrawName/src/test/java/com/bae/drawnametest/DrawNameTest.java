67a2c23be5cd86c584b3e35a03e6aca3f96dd322
Monika-Mistry <monika.mistry1@gmail.com>
2019-05-29 12:37:42 +0100

    set up test case

--- DrawName/src/test/java/com/bae/drawnametest/DrawNameTest.java
+++ DrawName/src/test/java/com/bae/drawnametest/DrawNameTest.java
@@ -1,5 +1,30 @@
 package com.bae.drawnametest;
 
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class DrawNameTest {
+
+	public static WebDriver driver;
+
+	@BeforeClass
+	public static void setup() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Administrator\\Documents\\chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+
+	@AfterClass
+	public static void teardown() {
+		driver.quit();
+	}
+
+	@Before
+	public static void init() {
+		driver.manage().window().maximize();
+		driver.get("https://www.youidraw.com/apps/painter/");
+	}
 
 }
