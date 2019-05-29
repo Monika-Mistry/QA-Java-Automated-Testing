42c5370f94c0e3bd6ea037632d7c1d5e19b854f8
Monika-Mistry <monika.mistry1@gmail.com>
2019-05-29 16:10:57 +0100

    Updated shopping tests

--- AutomatedTesting/src/test/java/com/bae/automatedtests/ShoppingWebsiteTest.java
+++ AutomatedTesting/src/test/java/com/bae/automatedtests/ShoppingWebsiteTest.java
@@ -1,12 +1,15 @@
 package com.bae.automatedtests;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -33,11 +36,71 @@ public class ShoppingWebsiteTest {
 	}
 
 	@Test
-	public void search() {
+	public void searchBar() {
 		WebElement searchBar = driver.findElement(By.name("search_query"));
-		// WebElement searchBtn = driver.findElement(By.name("submit_search"));
 
 		searchBar.click();
 		assertTrue(searchBar.isEnabled());
+	}
+
+	@Test
+	public void search() {
+		WebElement searchBar = driver.findElement(By.name("search_query"));
+
+		searchBar.click();
+		searchBar.sendKeys("dress");
+		searchBar.sendKeys(Keys.ENTER);
+
+		WebElement result = driver.findElement(By.xpath("//*[@id=\"center_column\"]/h1/span[1]"));
+
+		assertEquals("\"dress\"", result.getText().toLowerCase());
+
+	}
+
+	@Test
+	public void findItem() {
+
+		WebElement searchBar = driver.findElement(By.name("search_query"));
+
+		searchBar.click();
+		searchBar.sendKeys("dress");
+		searchBar.sendKeys(Keys.ENTER);
+		WebElement item = driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]/div/div[2]/h5/a"));
+
+		assertTrue(item.getText().contains("Printed Summer Dress"));
+	}
+
+	@Test
+	public void selectItem() {
+
+		WebElement searchBar = driver.findElement(By.name("search_query"));
+
+		searchBar.click();
+		searchBar.sendKeys("dress");
+		searchBar.sendKeys(Keys.ENTER);
+		WebElement select = driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]/div/div[2]/h5/a"));
+		select.click();
+		WebElement header = driver.findElement(By.xpath("//*[@id=\"center_column\"]/div/div/div[3]/h1"));
+		assertTrue(header.getText().contains("Printed Summer Dress"));
+	}
+
+	@Ignore
+	@Test
+	public void addItemToCart() {
+
+		WebElement searchBar = driver.findElement(By.name("search_query"));
+
+		searchBar.click();
+		searchBar.sendKeys("dress");
+		searchBar.sendKeys(Keys.ENTER);
+		WebElement select = driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li[1]/div/div[2]/h5/a"));
+		select.click();
+
+		WebElement addCart = driver.findElement(By.xpath("//*[@id=\"add_to_cart\"]/button"));
+		addCart.click();
+
+		WebElement success = driver.findElement(By.xpath("//*[@id=\"layer_cart\"]/div[1]/div[1]/h2"));
+		assertEquals("product successfully added to your shopping cart", success.getText().toLowerCase().trim());
+
 	}
 }
