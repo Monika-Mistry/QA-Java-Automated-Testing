584c2102c08bcd672799e63abd26d42fd2a4755c
Monika-Mistry <monika.mistry1@gmail.com>
2019-05-29 14:06:58 +0100

    test cases updated

--- DrawName/src/test/java/com/bae/drawnametest/DrawNameTest.java
+++ DrawName/src/test/java/com/bae/drawnametest/DrawNameTest.java
@@ -3,12 +3,18 @@ package com.bae.drawnametest;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 public class DrawNameTest {
 
 	public static WebDriver driver;
+	public Actions action;
 
 	@BeforeClass
 	public static void setup() {
@@ -22,9 +28,82 @@ public class DrawNameTest {
 	}
 
 	@Before
-	public static void init() {
+	public void init() {
 		driver.manage().window().maximize();
 		driver.get("https://www.youidraw.com/apps/painter/");
+		action = new Actions(driver);
+
 	}
+
+	@Ignore
+	@Test
+	public void changeBrush() throws InterruptedException {
+		WebElement brushType = driver.findElement(By.id("cmbPencil"));
+		WebElement fur = driver.findElement(By.xpath("//option[@value=\"Fur\"]"));
+		action.click(brushType).click(fur).perform();
+
+		Thread.sleep(2000);
+	}
+
+	@Ignore
+	@Test
+	public void drawM() throws InterruptedException {
+
+		WebElement canvas = driver.findElement(By.id("catch"));
+		action.moveToElement(canvas).clickAndHold().moveByOffset(0, -100).moveByOffset(50, 50).moveByOffset(50, -50)
+				.moveByOffset(0, 100).perform();
+		Thread.sleep(2000);
+	}
+
+	@Ignore
+	@Test
+	public void drawO() throws InterruptedException {
+		WebElement canvas = driver.findElement(By.id("catch"));
+		action.moveToElement(canvas).clickAndHold().moveByOffset(0, -100).moveByOffset(50, 0).moveByOffset(0, 100)
+				.moveByOffset(-50, 0).perform();
+		Thread.sleep(5000);
+	}
+
+	@Ignore
+	@Test
+	public void drawN() throws InterruptedException {
+		WebElement canvas = driver.findElement(By.id("catch"));
+		action.moveToElement(canvas).clickAndHold().moveByOffset(0, -100).moveByOffset(50, 100).moveByOffset(0, -100)
+				.perform();
+		Thread.sleep(2000);
+	}
+
+	@Ignore
+	@Test
+	public void drawI() throws InterruptedException {
+		WebElement canvas = driver.findElement(By.id("catch"));
+		action.moveToElement(canvas).clickAndHold().moveByOffset(0, -100).perform();
+
+		Thread.sleep(2000);
+	}
+
+	@Ignore
+	@Test
+	public void drawK() throws InterruptedException {
+		WebElement canvas = driver.findElement(By.id("catch"));
+		action.moveToElement(canvas).clickAndHold().moveByOffset(0, -100).moveByOffset(50, 50).moveByOffset(50, -50)
+				.moveByOffset(0, 100).perform();
+		Thread.sleep(2000);
+	}
+
+	@Ignore
+	@Test
+	public void drawA() throws InterruptedException {
+		WebElement canvas = driver.findElement(By.id("catch"));
+		action.moveToElement(canvas).clickAndHold().moveByOffset(0, -100).moveByOffset(50, 50).moveByOffset(50, -50)
+				.moveByOffset(0, 100).perform();
+		Thread.sleep(2000);
+
+	}
+
+	// @After
+	// public static void finalise() {
+	//
+	// }
 
 }
