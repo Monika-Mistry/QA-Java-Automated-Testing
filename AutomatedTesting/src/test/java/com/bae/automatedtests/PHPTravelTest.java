3cff50930b7b60626f727cee9a3f6cec14c7727a
Monika-Mistry <monika.mistry1@gmail.com>
2019-05-30 16:45:27 +0100

    adding surefire to pom, updated test class names

--- AutomatedTesting/src/test/java/com/bae/automatedtests/PHPTravelTest.java
+++ AutomatedTesting/src/test/java/com/bae/automatedtests/PHPTravelTest.java
@@ -15,7 +15,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-public class PHPTravelTests {
+public class PHPTravelTest {
 
 	public static WebDriver driver;
 
